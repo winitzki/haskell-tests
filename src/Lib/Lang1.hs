beab577d7b5c51babef1622ed547c50a07127ed9
Sergei Winitzki <winitzki@gmail.com>
2025-03-30 22:18:50 +0200

    wip starting on dtalc implementation

--- src/Lib/Lang1.hs
+++ src/Lib/Lang1.hs
@@ -1,55 +1,69 @@
-module Lib.Lang1 ( VarRef(..), Statement(..), Exp(..),   demo, sample, GetInts(..)) where
+module Lib.Lang1 where
+
+-- ( VarRef(..), Statement(..), Exp(..),   demo, sample, GetInts(..))
+
 -- Data types à la carte: exercises.
 
 -- Begin with the toy language expressed via direct recursion.
 
-data VarRef = Var String
-            | FieldRef VarRef String
+data VarRef
+  = Var String
+  | FieldRef VarRef String
 
-data Statement = Assign VarRef Exp
-               | Seq Statement Statement
+data Statement
+  = Assign VarRef Exp
+  | Seq Statement Statement
 
-data Exp = Mul Exp Exp
-         | IntExp Int
-         | VarExp VarRef
+data Exp
+  = Mul Exp Exp
+  | IntExp Int
+  | VarExp VarRef
 
 -- Convert the language to String. This is done via the Show typeclass.
 
 instance Show VarRef where
-    show (Var s) = s
-    show (FieldRef r s) = show r ++ "." ++ s
+  show (Var s) = s
+  show (FieldRef r s) = show r ++ "." ++ s
 
 instance Show Statement where
-    show (Assign v e) = show v ++ " := " ++ show e ++ ";"
-    show (Seq s1 s2) = show s1 ++ "\n" ++ show s2
+  show (Assign v e) = show v ++ " := " ++ show e ++ ";"
+  show (Seq s1 s2) = show s1 ++ "\n" ++ show s2
 
 instance Show Exp where
-    show (Mul e1 e2) = show e1 ++ " * " ++ show e2
-    show (IntExp i) = show i
-    show (VarExp v) = show v
+  show (Mul e1 e2) = show e1 ++ " * " ++ show e2
+  show (IntExp i) = show i
+  show (VarExp v) = show v
 
 -- Extract all literal integers from the language.
 
 class GetInts a where
-    getInts :: a -> [Int]
+  getInts :: a -> [Int]
 
 instance GetInts VarRef where
-    getInts _ = []
+  getInts _ = []
 
 instance GetInts Exp where
-    getInts (IntExp i) = [i]
-    getInts (Mul e1 e2) = getInts e1 ++ getInts e2
-    getInts _ = [] 
+  getInts (IntExp i) = [i]
+  getInts (Mul e1 e2) = getInts e1 ++ getInts e2
+  getInts _ = []
 
 instance GetInts Statement where
-    getInts (Assign _ e) = getInts e
-    getInts (Seq s1 s2) = getInts s1 ++ getInts s2
+  getInts (Assign _ e) = getInts e
+  getInts (Seq s1 s2) = getInts s1 ++ getInts s2
 
 sample :: Statement
-sample = Seq (Assign (Var "a") (IntExp 1)) (Assign (Var "b") (Mul  (IntExp 2) (VarExp (Var "a"))))
+sample = Seq (Assign (Var "a") (IntExp 1)) (Assign (Var "b") (Mul (IntExp 2) (VarExp (Var "a"))))
 
 demo :: IO ()
 demo = do
-    print sample
+  print sample
 
 -- Implement the same language using DTALC techniques. Implement isomorphism between both types.
+
+-- Pattern functors need to take 3 arguments since we are defining 3 mutually recursive types at once:
+
+data VarRefP r = VarP String | FieldRefP r String
+
+data StatementP r varRefP expP = AssignP varRefP expP | SeqP r r
+
+data ExpP r varRefP = MulP r r | IntExpP Int | VarExpP varRefP
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,8 +1,8 @@
 -- module Spec (main)  where
--- It is an error to define a module here!! 
+-- It is an error to define a module here!!
 
+import Lib.Lang1 -- (GetInts(..), Exp(..), VarRef(..), sample)  -- This would be an explicit export list.
 import Test.Hspec
-import Lib.Lang1  (GetInts(..), Exp(..), VarRef(..), sample)
 
 main :: IO ()
 main = hspec $ do
@@ -11,7 +11,7 @@ main = hspec $ do
       show sample `shouldBe` "a := 1;\nb := 2 * a;"
 
     it "shows a multiplication" $ do
-      show (Mul  (IntExp 2) (VarExp (Var "a"))) `shouldBe` "2 * a"
+      show (Mul (IntExp 2) (VarExp (Var "a"))) `shouldBe` "2 * a"
 
     it "extracts integers" $ do
-      getInts sample `shouldBe` [1 , 2 ]
+      getInts sample `shouldBe` [1, 2]
